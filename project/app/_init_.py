7fdaaac31189a2e8743d7733ce65f676e9ef698a
Cristian M <crismur93@gmail.com>
2022-06-16 11:20:33 -0500

    front end first commit

--- project/app/_init_.py
+++
@@ -1,3 +0,0 @@
-app = dash.Dash(__name__)
-if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=True, threaded=True)
--- project/app/app.py
+++ project/app/app.py
@@ -6,24 +6,175 @@ from dash.dependencies import Input, Output
 from functions import plot_regression
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "Credit Risk Estimator"
+
+
 server = app.server
 
-app.layout = html.Div(
+
+def dashboard_menu():
+    """
+    :return: Div containing dashboard Overview, Loans, History & Settings
+    """
+    return html.Div(
+        id="dashboard-menu",
+        children=[
+            html.H4("Dashboard"),
+            dbc.Button("Overview", color="Light", style={"text-align": "left"}),
+            dbc.Button("Loans", color="Light", style={"text-align": "left"}),
+            dbc.Button("History", color="Light", style={"text-align": "left"}),
+            dbc.Button("Settings", color="Light", style={"text-align": "left"}),
+            html.Div(
+                id="intro",
+                children="Explore this app 2."
+            ),
+        ],
+        className="d-grid gap-2",
+    )
+
+cards = [
+    dbc.Card(
+        [
+            html.H3("Overview", style={"color": "white", "padding":"16px"})
+        ],
+        color="#F44336"
+    ),
+    dbc.Card(
+        [
+            html.H3("Loans Given", style={"color": "white", "padding":"16px"}),
+            html.H3("Loans Taken", style={"color": "white", "padding":"16px"})
+        ],
+        color="#2196F3"
+    ),
+    dbc.Card(
+        [
+            html.H3("History", style={"color": "white"})
+        ],
+        color="#009688"
+    ),
+]
+
+def stats_table():
+    table_rows = [html.Tr([html.Td("String"), html.Td("Number")]) for i in range(6)]
+    table_body = [html.Tbody(table_rows)]
+    return dbc.Table(table_body, bordered=True)
+
+more_data_btn = html.Div(
     [
+        dbc.Button(
+            "More data", id="more_data_btn", color="secondary"
+        )
+    ]
+)
+
+app.layout = html.Div(
+    id="app-container",
+    style={"margin": "10px"},
+    children=[
+        # Banner
         html.Div(
-            [
-                dcc.Graph(id="regression_plot"),
-                html.P(
-                    "Standard Deviation", style={"color": "white", "marginLeft": "20px"}
+            id="banner",
+            className="banner",
+            children=[html.Img(src=app.get_asset_url("plotly_logo.png"))],
+        ),
+        dbc.Row(
+            [dbc.Col(
+                children=[html.P("Welcome, ", style={"fontSize": 14}), 
+                 html.Strong("User", style={"color": "red"})],
+                width=2,
+                style={"padding": "auto"}
+            ),
+            dbc.Col(
+                html.H4("My Dashboard", style={"color": "blue"}),
+                width=10
+            )]
+        ),
+        dbc.Row(
+            id="row1",
+            children=[
+                # Left column
+                dbc.Col(
+                    dashboard_menu(),
+                    # + [
+                    #    html.Div(
+                    #        ["initial child"], id="output-clientside", style={"display": "none"}
+                    #    )
+                    # ],
+                    width=2,
                 ),
-                dcc.Slider(
-                    id="std_slider",
-                    min=0,
-                    max=40,
-                    step=0.5,
-                    value=10,
-                    marks={i: str(i) for i in range(0, 40, 5)},
-                ),
+                # Right column
+                dbc.Col(
+                    [
+                    dbc.Row(
+                        id="upper-stats",
+                        children=[dbc.Col(card) for card in cards],
+                        style={"margin": "10px"}
+                    ),
+                    dbc.Row(
+                        id="middle-stats",
+                        children=[
+                            html.Div(
+                                id="plot_container",
+                                children=[
+                                    dcc.Graph(id="regression_plot"),
+                                    html.P(
+                                        "Standard Deviation", style={"color": "white", "marginLeft": "20px"}
+                                    ),
+                                    dcc.Slider(
+                                        id="std_slider",
+                                        min=0,
+                                        max=40,
+                                        step=0.5,
+                                        value=10,
+                                        marks={i: str(i)
+                                               for i in range(0, 40, 5)},
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        [html.H5("General Status"),
+                        html.P("Credit Score"),
+                        html.P("Loans available")]
+                    ),
+                    html.Div(
+                        [html.H5("Data"),
+                        stats_table(),
+                        more_data_btn]
+                    ),
+                    html.Hr(),
+                    html.Div(
+                        id="lower-stats",
+                        style={"background-color": "#616161", "padding": "32px 16px"},
+                        children=[
+                            dbc.Row(
+                                [dbc.Col(
+                                    [
+                                    html.H5("Demographic"),
+                                    html.P("Income"),
+                                    html.P("Loans"),
+                                    html.P("Funded"),
+                                    ]
+                                ),
+                                dbc.Col(
+                                    [html.H5(""),
+                                    html.P("Settlement"),
+                                    html.P("Term"),
+                                    html.P("Rate"),]
+                                ),
+                                dbc.Col(
+                                    [html.H5("Target Users"),
+                                    html.P("Users"),
+                                    html.P("Active"),
+                                    html.P("HeatMap"),]
+                                )],
+                            )
+                        ]
+                    ),
+                    ],
+                    width=10
+                )
             ]
         ),
     ]
@@ -36,3 +187,7 @@ app.layout = html.Div(
 )
 def update_regression_plot(std):
     return plot_regression(std)
+
+
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
