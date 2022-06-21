cf6f7a41c8a8b14a1a010151ea496d34c04330f7
Cristian M <crismur93@gmail.com>
2022-06-20 21:30:24 -0500

    adding components

--- project/app.py
+++ project/app.py
@@ -1,15 +1,23 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc
-from dash import html
+from dash import dcc, html
 from dash.dependencies import Input, Output
-from app.functions import get_user_stats, loan_balance, plot_regression
+import dash_labs as dl
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+from callbacks import register_callbacks
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], plugins=[dl.plugins.pages])
 app.title = "Credit Risk Estimator"
 
 
 server = app.server
+
+
+#navbar = dbc.NavbarSimple([
+#        dbc.NavItem(dbc.NavLink(page['name'], href=page['path']))
+#        for page in dash.page_registry.values()
+#        if page["module"] != "pages.not_found_404"
+#], brand='Dash App')
 
 
 def dashboard_menu():
@@ -20,10 +28,10 @@ def dashboard_menu():
         id="dashboard-menu",
         children=[
             html.H5("Dashboard"),
-            dbc.Button("Overview", color="Light", className="menu-btns"),
-            dbc.Button("Your Scorecard", color="Light", className="menu-btns"),
+            dbc.Button("Overview", color="Light", className="menu-btns", href="/overview"),
+            dbc.Button("Your Scorecard", color="Light", className="menu-btns", href="/scorecard"),
             dbc.Button("History - Stats", color="Light", className="menu-btns"),
-            dbc.Button("About", color="Light", className="menu-btns"),
+            dbc.Button("About", color="Light", className="menu-btns", href="/about"),
         ],
         className="d-grid gap-2",
     )
@@ -57,57 +65,6 @@ def info_cards():
     return dbc.Row(
         id="upper-stats",
         children=[dbc.Col(card) for card in cards]
-    )
-
-
-def stats_table():
-    more_data_btn = html.Div([dbc.Button("More data >", id="more_data_btn", color="secondary")])
-    table_rows = [html.Tr([html.Td(k), html.Td(f"{v:>.2f}%")]) for k, v in get_user_stats().items()]
-    table_body = [html.Tbody(table_rows)]
-
-    return html.Div(
-            [html.H5("Data"),
-            dbc.Table(table_body, bordered=True, hover=True),
-            more_data_btn]
-            )
-
-
-def middle_stats():
-    return dbc.Row(
-        id="middle-stats",
-        children=[
-            html.Div(
-                id="plot_container",
-                children=[
-                    dcc.Graph(id="regression_plot"),
-                    html.P(
-                        "Standard Deviation", style={"color": "white", "marginLeft": "20px"}
-                    ),
-                    dcc.Slider(
-                        id="std_slider",
-                        min=0,
-                        max=40,
-                        step=0.5,
-                        value=10,
-                        marks={i: str(i)
-                               for i in range(0, 40, 5)},
-                    ),
-                ]
-            ),
-        ]
-    )
-
-
-def bar_stats():
-    credit_score, score_increase, remaining_debt, original_loan = loan_balance().values()
-    return html.Div(
-        [
-        html.H5("General Stats"),
-        html.P(f"{credit_score} Credit Score"),
-        dbc.Progress(label=f"+{score_increase}%", style={"height": "40px"}, value=score_increase, color="success"),
-        html.P("Loans available"),
-        dbc.Progress(label=f"USD {remaining_debt}/{original_loan}", style={"height": "40px"}, value=(int(remaining_debt/original_loan*100)), color="danger"),
-        ],
     )
 
 
@@ -155,6 +112,7 @@ app.layout = html.Div(
             className="banner",
             children=[html.Img(src=app.get_asset_url("plotly_logo.png"))],
         ),
+        #navbar,
         dbc.Row(
             [dbc.Col(
                 children=[html.P("Welcome, User", style={"fontSize": 14}), ],
@@ -179,9 +137,7 @@ app.layout = html.Div(
                 dbc.Col(
                     [
                     info_cards(),
-                    middle_stats(),
-                    bar_stats(),
-                    stats_table(),
+                    dl.plugins.page_container,
                     html.Hr(),
                     lower_stats()],
                     width=10
@@ -192,12 +148,7 @@ app.layout = html.Div(
 )
 
 
-@app.callback(
-    Output(component_id="regression_plot", component_property="figure"),
-    [Input(component_id="std_slider", component_property="value")],
-)
-def update_regression_plot(std):
-    return plot_regression(std)
+register_callbacks(app)
 
 
 if __name__ == "__main__":
