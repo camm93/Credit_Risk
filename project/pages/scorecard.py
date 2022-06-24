9500b28d33cb9398b26e1e9993bef773fb270a3e
aleg448 <90354487+aleg448@users.noreply.github.com>
2022-06-23 22:27:14 -0500

    Update scorecard.py

--- project/pages/scorecard.py
+++ project/pages/scorecard.py
@@ -7,29 +7,24 @@ import dash_bootstrap_components as dbc
 register_page(__name__, path='/scorecard')
 
 def input_form():
-        return dbc.Form(
-                [
-                        dbc.FormGroup(
-                                [
-                                        dbc.Label("Loan Amount"),
-                                        dbc.Input(type="number", id="loan_amount", value=100000),
-                                        dbc.Label("Loan Duration"),
-                                        dbc.Input(type="number", id="loan_duration", value=30),
-                                        dbc.Label("Interest Rate"),
-                                        dbc.Input(type="number", id="interest_rate", value=5),
-                                        dbc.Label("Loan Purpose"),
-                                        dbc.Input(type="text", id="loan_purpose", value="Home Improvement"),
-                                        dbc.Label("Loan Type"),
-                                        dbc.Input(type="text", id="loan_type", value="Conventional"),
-                                ]
-                        ),
-                        dbc.Button("Submit", id="submit_btn", color="primary"),
-                ]
-        )
+        return dbc.Form(children=[       dbc.Label("Loan Amount"),
+                                dbc.Input(type="number", id="loan_amount", value=100000),
+                                dbc.Label("Loan Duration"),
+                                dbc.Input(type="number", id="loan_duration", value=30),
+                                dbc.Label("Interest Rate"),
+                                dbc.Input(type="number", id="interest_rate", value=5),
+                                dbc.Label("Loan Purpose"),
+                                dbc.Input(type="text", id="loan_purpose", value="Home Improvement"),
+                                dbc.Label("Loan Type"),
+                                dbc.Input(type="text", id="loan_type", value="Conventional"),
+                                dbc.Button("Submit", id="submit_btn", color="primary"),
+
+])
+user_stats={}
 
 def data_table():
         clean_data = html.Div([dbc.Button("Clear information", id="clean_data", color="secondary")])
-        table_rows = [html.Tr([html.Td(k), html.Td(f"{v:>.2f}%")]) for k, v in get_user_stats().items()]
+        table_rows = [html.Tr([html.Td(k), html.Td(v)]) for k, v in user_stats.items()]
         table_body = [html.Tbody(table_rows)]
         return html.Div(
                 [html.H5("Data"),
@@ -41,6 +36,3 @@ layout = html.Div([
         input_form(),
         data_table(),
 ])
-        
-
-                        
