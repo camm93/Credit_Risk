0585ab1499bf2489d06d0d8ea423b7e38e6969c0
Cristian M <crismur93@gmail.com>
2022-07-07 10:06:58 -0500

    deployment changes two

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ app.layout = html.Div(
 )
 
 
-# if __name__ == "__main__":
-#     app.run_server(host="0.0.0.0", port=8050, debug=True)
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
